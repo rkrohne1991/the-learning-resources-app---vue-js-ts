6759ddceacfbf120c5ed3e3f2abb1745e60ce646
Rafal <rafal.krohne@hotmail.co.uk>
2022-06-07 07:23:30 +0200

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <header>
+    <h1>The Learning Resources App</h1>
+  </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  data() {
+    return {};
+  },
+});
+</script>
