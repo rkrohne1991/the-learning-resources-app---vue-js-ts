828b36218097c158f767e27773fcaa1055a40635
Rafal <rafal.krohne@hotmail.co.uk>
2022-06-09 08:32:15 +0200

    Setting up vuetify and sass with few config changes

---
+++ src/plugins/vuetify.ts
@@ -0,0 +1,7 @@
+import 'vuetify/lib/styles/main.sass';
+
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/lib/components/index';
+import * as directives from 'vuetify/lib/directives/index';
+
+export const vuetify = createVuetify({ components, directives });
