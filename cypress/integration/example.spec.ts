18be9c8e51b55457286a636d697093f689417bd6
Rafal <rafal.krohne@hotmail.co.uk>
2022-06-15 14:29:24 +0200

    Test adjustment

--- cypress/integration/example.spec.ts
+++ cypress/integration/example.spec.ts
@@ -3,6 +3,6 @@
 describe('My First Test', () => {
   it('visits the app root url', () => {
     cy.visit('/');
-    cy.contains('.v-toolbar-title__placeholder', 'The Learning Resources App');
+    cy.contains('header', 'The Learning Resources App');
   });
 });
